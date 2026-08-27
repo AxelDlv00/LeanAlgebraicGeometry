17cd88828cbfb8e233344afbad06f63b51445b60
Archon Horizon <archon@archon-horizon.local>
2026-08-28 01:23:21 +0800

    feat(stacks-part07): formalize products of pullback squares
    Summary: Add componentwise product pullback infrastructure
    Archon-Commit: agent
    Archon-Run: 0188
    Archon-Role: horizon
    Archon-Session: 0004-horizon-fs-stacks-part07-alg-stacks
    Archon-Task: fs-stacks-part07-alg-stacks
    Archon-Projects: StacksPart07_AlgebraicStacks

--- FormalizedSources/StacksProject/Part07_AlgebraicStacks/StacksPart07Lib/ProductRepresentability.lean
+++ FormalizedSources/StacksProject/Part07_AlgebraicStacks/StacksPart07Lib/ProductRepresentability.lean
@@ -21,7 +21,7 @@ open CategoryTheory
 open CategoryTheory.Limits
 open CategoryTheory.MorphismProperty
 
-universe v u
+universe v u v' u'
 
 /-! The pullback of a map along the second product projection. -/
 
@@ -48,6 +48,78 @@ lemma isPullback_prod_snd_with_id {C : Type u} [Category.{v} C]
       apply prod.hom_ext
       · rw [← h₂, Category.assoc, prod.map_fst, prod.lift_fst, Category.comp_id]
       · rw [← h₁, prod.lift_snd])
+
+/-! ### Products of pullback squares -/
+
+/-- The product of two pullback squares is a pullback square.
+
+The universal property is checked componentwise in the two factor
+categories. -/
+lemma isPullback_prod_category
+    {C : Type u} [Category.{v} C] {D : Type u'} [Category.{v'} D]
+    {P X Y Z : C} {P' X' Y' Z' : D}
+    {fst : P ⟶ X} {snd : P ⟶ Y} {f : X ⟶ Z} {g : Y ⟶ Z}
+    {fst' : P' ⟶ X'} {snd' : P' ⟶ Y'} {f' : X' ⟶ Z'} {g' : Y' ⟶ Z'}
+    (h : IsPullback fst snd f g) (h' : IsPullback fst' snd' f' g') :
+    IsPullback (Prod.mkHom fst fst') (Prod.mkHom snd snd')
+      (Prod.mkHom f f') (Prod.mkHom g g') := by
+  let c : PullbackCone (Prod.mkHom f f') (Prod.mkHom g g') :=
+    PullbackCone.mk (Prod.mkHom fst fst') (Prod.mkHom snd snd')
+      (by
+        apply Prod.hom_ext
+        · exact h.w
+        · exact h'.w)
+  apply IsPullback.of_isLimit (c := c)
+  exact PullbackCone.IsLimit.mk c.condition
+    (fun s => Prod.mkHom
+      (h.lift s.fst.1 s.snd.1 (by
+        exact congrArg Prod.fst s.condition))
+      (h'.lift s.fst.2 s.snd.2 (by
+        exact congrArg Prod.snd s.condition)))
+    (fun s => by
+      apply Prod.hom_ext
+      · change
+          h.lift s.fst.1 s.snd.1 (congrArg Prod.fst s.condition) ≫ fst = s.fst.1
+        exact h.lift_fst _ _ _
+      · change
+          h'.lift s.fst.2 s.snd.2 (congrArg Prod.snd s.condition) ≫ fst' = s.fst.2
+        exact h'.lift_fst _ _ _)
+    (fun s => by
+      apply Prod.hom_ext
+      · change
+          h.lift s.fst.1 s.snd.1 (congrArg Prod.fst s.condition) ≫ snd = s.snd.1
+        exact h.lift_snd _ _ _
+      · change
+          h'.lift s.fst.2 s.snd.2 (congrArg Prod.snd s.condition) ≫ snd' = s.snd.2
+        exact h'.lift_snd _ _ _)
+    (fun s m hm₁ hm₂ => by
+      apply Prod.hom_ext
+      · apply h.hom_ext
+        · have hm₁' : m.1 ≫ fst = s.fst.1 := by
+            simpa [c] using congrArg Prod.fst hm₁
+          have hl₁' :
+              (h.lift s.fst.1 s.snd.1 (congrArg Prod.fst s.condition)) ≫ fst = s.fst.1 := by
+            exact h.lift_fst _ _ _
+          exact hm₁'.trans hl₁'.symm
+        · have hm₂' : m.1 ≫ snd = s.snd.1 := by
+            simpa [c] using congrArg Prod.fst hm₂
+          have hl₂' :
+              (h.lift s.fst.1 s.snd.1 (congrArg Prod.fst s.condition)) ≫ snd = s.snd.1 := by
+            exact h.lift_snd _ _ _
+          exact hm₂'.trans hl₂'.symm
+      · apply h'.hom_ext
+        · have hm₁' : m.2 ≫ fst' = s.fst.2 := by
+            simpa [c] using congrArg Prod.snd hm₁
+          have hl₁' :
+              (h'.lift s.fst.2 s.snd.2 (congrArg Prod.snd s.condition)) ≫ fst' = s.fst.2 := by
+            exact h'.lift_fst _ _ _
+          exact hm₁'.trans hl₁'.symm
+        · have hm₂' : m.2 ≫ snd' = s.snd.2 := by
+            simpa [c] using congrArg Prod.snd hm₂
+          have hl₂' :
+              (h'.lift s.fst.2 s.snd.2 (congrArg Prod.snd s.condition)) ≫ snd' = s.snd.2 := by
+            exact h'.lift_snd _ _ _
+          exact hm₂'.trans hl₂'.symm)
 
 /-!
 ### Product representability
