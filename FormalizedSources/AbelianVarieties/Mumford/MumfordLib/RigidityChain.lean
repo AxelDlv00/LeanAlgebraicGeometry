0ac2c72757b31ca169b252ffcc8bd5c44a7131df
Archon Horizon <archon@archon-horizon.local>
2026-08-29 17:23:39 +0800

    chore(mumford): fix rigidity module headers
    Archon-Commit: agent
    Archon-Run: 0197
    Archon-Role: horizon
    Archon-Session: 0002-horizon-fs-mumford
    Archon-Task: fs-mumford
    Archon-Projects: Mumford

--- FormalizedSources/AbelianVarieties/Mumford/MumfordLib/RigidityChain.lean
+++ FormalizedSources/AbelianVarieties/Mumford/MumfordLib/RigidityChain.lean
@@ -1,6 +1,6 @@
 /-
 Copyright (c) 2026 The Mumford Contributors. All rights reserved.
-Released under Apache 2.0 license as described in the file LICENSE
+Released under Apache 2.0 license as described in the file LICENSE.
 Authors: The Mumford Contributors
 -/
 
@@ -237,4 +237,3 @@ end RigidityChain
 
 end GroupScheme
 end Mumford
-
